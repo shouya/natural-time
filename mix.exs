e78d7a45d0f2933fd83ce1003b5d5c0cc6707069
Shou Ya <shouya@users.noreply.github.com>
2021-07-09 22:18:42 +0800

    prepare publishing to hex

--- mix.exs
+++ mix.exs
@@ -6,23 +6,36 @@ defmodule NaturalTime.MixProject do
       app: :natural_time,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: "A simple parser for datetime in natural language",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:timex, "~> 3.5"},
-      {:nimble_parsec, "~> 1.1"}
+      {:nimble_parsec, "~> 1.1"},
+      # neeeded for publishing to hex
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "natural_time",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["MIT"],
+      links: %{
+        "github" => "https://github.com/shouya/natural-time"
+      }
     ]
   end
 end
